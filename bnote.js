2618534a49daddce4cdf36404314ab0c5dfaf382
Mohamed Sawy <mohamedsawy1022@gmail.com>
2023-08-24 11:28:12 +0300

    Add new packages and add validation for environment variables

--- bnote.js
+++ bnote.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
 import 'dotenv/config';
-import './src/commands/index.mjs';
+import chalk from 'chalk';
 
+
+const requiredEnvVars = [
+    process.env.DB_CONNECTION_STRING,
+    process.env.SNOTES_PASSWORD
+];
+
+if (requiredEnvVars.every(exists)) {
+    start();
+} else {
+    console.error(chalk.red("Error: Environment is not configured correctly"));
+}
+
+
+function exists(variable) {
+    return variable
+}
+
+async function start() {
+    import('./src/commands/index.mjs');
+}
